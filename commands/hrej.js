f753bc0f653f5b40efbf7893b88bc9000457483a
warrenek <jirka.bartunek@gmail.com>
2020-01-13 19:30:12 +0100

    last version

--- commands/hrej.js
+++ commands/hrej.js
@@ -8,112 +8,76 @@ const youtube = google.youtube({
 });
 
 async function GetListYTB(searchInput) {
-    console.log('zactek GetListYTB');
-      let res = await youtube.search.list({
+    console.log(`zactek GetListYTB:${searchInput}`);
+      return youtube.search.list({
        part: 'id,snippet',
        q: searchInput,
        type: 'video',
-       maxResults:10
-     })
-    //  .then(function(response) {
-    //     // pockam si, nez to najdu
-    //     console.log(JSON.stringify(response, null, 1));
-    //     return JSON.stringify(response, null, 1);
-    //   },
-    //   function(err) { console.log("Execute error", err); });
-    let kurwa = res;
-    return JSON.stringify(res, null, 1);
-   
+       maxResults:10,
+       order:'viewCount'
+     })   
 };
 
 
 async function execute(client, message, args){
-   if(!message.member.voiceChannel) return message.channel.send("Připoj se k táboráku.");
+   if(!message.member.voiceChannel) return message.channel.send("Připoj se k slečinkám!"); 
    console.log(`kontrola chanu`);
 
    if(!args[0]) return  message.channel.send("Nic si nevložil máslo."); 
    console.log(`kontrola argumetnu`);
 
-   let validate = await ytdl.validateURL(args[0])
-
-   //if(!validate) return  message.channel.send("Nic nepřehraju....to není URL, SORRY JAKO.");
-   //console.log(`valide argumetu: ${args[0]}`)
-
-    let ytbAPIresponse = await GetListYTB(args[0]);
-    console.log('ytbAPIresponse',ytbAPIresponse.response);
+    let ytbAPIresponse = await GetListYTB(args.join(' '));
+    let ytbPref = 'https://www.youtube.com/watch?v='+'';
+    arr = [];
     
-    const obj = JSON.parse(await GetListYTB(args[0]));
-    console.log(obj.items);
+    console.log(`tu ${ytbAPIresponse.data.items.find(c=>c.id.videoId).toString()}`);
 
-    // Object.keys(ytbAPIresponse).forEach(e =>{
-    //     field.push(ytbAPIresponse.items[0]);
-    //     field2.push(e)
-    //     counter++;
-        
-    //     console.log(ytbAPIresponse.items[0]);
-    // });
+    ytbAPIresponse.data.items.forEach(function(item, index, array) {
+        arr.push(ytbPref + item.id.videoId + ';' + item.snippet.title + ' ' + item.snippet.description);
+        console.log(ytbPref + item.id.videoId);       
+        })
 
-    // console.log(field);
-    // console.log(field2);
+        if(arr.length==0){
+        return  message.channel.send("Nic sem nenašel ..znovu a lépe.");
+        }
 
+    console.log('nalezeno:' + arr.length);
+        // vezmu prni nejsledovanejsi link
+    let urlLink = arr[0].split(';')[0];
+    let urlTitle = arr[0].split(';')[1];
+
+    //oznameni :)
     
 
-    // ytdl.getInfo(args[0], (err, info) => {
-    //     if (err) throw err;
-    //     let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-    //         console.log('Formats with only audio: ' + audioFormats.length);
-    //   });
+    console.log(urlLink, urlTitle);
 
-   //let info = await ytdl.getInfo(args[0]);
-   //console.log(`bagruju info z : ${info}`);
+    let validate = await ytdl.validateURL(urlLink)
+
+    if(!validate) return  message.channel.send("Nic nepřehraju....to není URL, SORRY JAKO.");
+    console.log(`validace argumetu: ${urlLink}`)
+
 
    let connection = await message.member.voiceChannel.join();
-   console.log(`connection voice chanel ${args[0]}`);
+   console.log(`connection voice chanel ${connection}`);
    
-   //connection.playOpusStream(await ytdl(args[0]));
-   //connection.playStream(await ytdl(args[0], { filter: 'audioonly'}));
+  
+   message.channel.send(args.join(' ') + ' Mother fuckers, son of a bitches and other assholes', {
+    tts: true
+   }).then(connection.playStream(await ytdl(urlLink, { filter: 'audioandvideo'})));
+   
    message.member.voiceChannel.setTopic("Disko trisko","jedééém ať se práší za kočárem");
    //let dispetcher = await connection.playStream(ytdl(args[0], { filter: 'audioonly'}));
    //console.log(dispetcher);
 
-   //var minutes = Math.floor(info.player_response.videoDetails.lengthSeconds / 60);
-   //var secs = info.player_response.videoDetails.lengthSeconds % 60;
-
-   //message.channel.send(`Právě ti diskžokej: ${message.author} skladbu: ${info.title}`)
-   message.channel.send(`Právě ti hraje diskžokej: ${message.author}`);   
-
-   //zmizni
-
-   //waait(message);
    
+   message.channel.send(`Právě diskžokej: ${message.author} skladbu: ${urlTitle} hraje ti. [${urlLink.substring(0, 50)+'...'}]`);   
 
 }
-
-
-// ytsr.getFilters('github', function(err, filters) {
-//     if(err) throw err;
-//       filter = filters.get('Type').find(o => o.name === 'Video');
-//     ytsr.getFilters(filter.ref, function(err, filters) {
-//       if(err) throw err;
-//         filter = filters.get('Duration').find(o => o.name.startsWith('Short'));
-//         var options = {
-//             limit: 5,
-//             nextpageRef: filter.ref,
-//         }
-//         ytsr(null, options, function(err, searchResults) {
-//             if(err) throw err;
-//             dosth(searchResults);
-//         });
-//       });
-//   });
-  
-
-
-
 
 module.exports = {
     execute
 }
+
 
 
 
